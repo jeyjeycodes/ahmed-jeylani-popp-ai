519efd5ae72b951ad323098b0db7dd91941a4004
jeyjeycodes <83839350+jeylanicodes@users.noreply.github.com>
2024-06-18 19:34:41 +0100

    chore: added react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import '@/styles/global.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from '@/app.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/libs';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
